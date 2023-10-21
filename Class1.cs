ebf2b91b50f7047cd27d45130c65cb37d41b4090
Mariami <Mariami@MARIAMI_ASUS>
2023-10-21 15:37:53 +0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba4
+{
+    /*    
+     შექმენით საბაზო კლასი, რომელიც შეიცავს სამკუთხედის გვერდებს.ერთი
+    გვერდი არის ღია, მეორე და მესამე დაცული. შექმენით სამკუთხედის მემკვიდრე
+    კლასი, რომელიც დამატებით შეიცავს პრივატულ ცვლადებს - სამკუთხედის
+    პერიმეტრს და ფართობს; ასევე ორ ღია მეთოდს.პირველი მეთოდი გამოთვლის
+    და აბრუნებს სამკუთხედის ფართობს. მეორე მეთოდი გამოთვლის და გასცემს
+    სამკუთხედის პერიმეტრს.მთავარ პროგრამაში შექმენით მემკვიდრე კლასის
+    ტიპის მქონე ობიექტი და გამოიძახეთ ორივე მეთოდი.*/
+
+    class samkutxedi
+    {
+        public int gverdi1;
+        protected int gverdi2, gverdi3;
+    }
+    class memkvidre : samkutxedi
+    {
+        private int perimetri, fartobi;
+        public memkvidre(int par2,int par3)
+        {
+            gverdi2 = par2;
+            gverdi3 = par3;
+        }
+        public int metodi_1()
+        {
+            perimetri = gverdi1 + gverdi2 + gverdi3;
+            return perimetri;
+        }
+        public int metodi_2()
+        {
+            fartobi = (gverdi1 * gverdi2) / 2;
+            return fartobi;
+        }
+    }
+}
+
---
+++ davaleba 4 mariam bidzinashvili.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davaleba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            int ricxvi1=int.Parse(textBox1.Text);
+            int ricxvi2 = 4;
+            int ricxvi3 = 5;
+
+            memkvidre obieqti = new memkvidre(ricxvi2, ricxvi3);
+
+            obieqti.gverdi1 = ricxvi1;
+
+            int perimetri = obieqti.metodi_1();
+            int fartobi=obieqti.metodi_2();
+
+            label1.Text="PERIMETRIA  "+perimetri.ToString()+"   FARTOBIA  "+fartobi.ToString();
+        }
+    }
+}
